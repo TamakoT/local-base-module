d9b2f1c6c540ccd28d8858854047466b57786ba1
zhangtuo <zhangtuo98@126.com>
2023-06-08 16:47:43 +0800

    自动配置接口

--- local-framework/local-feign/src/main/java/com/tamako/feign/config/FeignAutoConfiguration.java
+++ local-framework/local-feign/src/main/java/com/tamako/feign/config/FeignAutoConfiguration.java
@@ -1,5 +1,8 @@
 package com.tamako.feign.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
 /**
  * feign自动配置类
  *
@@ -7,5 +10,9 @@ package com.tamako.feign.config;
  * @datetime 2023/6/8 15:53
  * @corporation 骎策科技（杭州）
  **/
+@Configuration
+@ComponentScan(basePackages = {
+        "com.tamako.feign.client"
+})
 public class FeignAutoConfiguration {
 }
